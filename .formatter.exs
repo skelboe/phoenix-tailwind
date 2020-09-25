ee2d39dc1bc2cd95a0ede01914cf2d2d7dfe2a53
Lars Skelboe <lars@skelboe.dk>
2020-09-25 12:27:34 +0200

    Add locals to formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  subdirectories: ["priv/*/migrations"],
+  locals_without_parens: [resource: 1, resource: 2, assert_resource: 2]
 ]
