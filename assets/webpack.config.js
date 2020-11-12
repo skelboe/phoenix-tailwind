8082bc0caeadb00a42b8d13bf1386d9e813abf40
Lars Skelboe <lars@skelboe.dk>
2020-11-12 18:23:32 +0100

    Update to webpack 5
    This also updates the rest of the dependencies

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,20 +1,16 @@
 const path = require("path");
 const glob = require("glob");
-const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = (env, options) => {
   const devMode = options.mode !== "production";
 
   return {
     optimization: {
-      minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({}),
-      ],
+      minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
     },
     entry: {
       app: glob.sync("./vendor/**/*.js").concat(["./js/app.js"]),
@@ -42,7 +38,9 @@ module.exports = (env, options) => {
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: "../css/app.css" }),
-      new CopyWebpackPlugin([{ from: "static/", to: "../" }]),
-    ].concat(devMode ? [new HardSourceWebpackPlugin()] : []),
+      new CopyWebpackPlugin({
+        patterns: [{ from: "static/", to: "../" }],
+      }),
+    ],
   };
 };
--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :myapp, MyAppWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
+      "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
